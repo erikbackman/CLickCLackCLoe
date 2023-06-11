58af6870bc27fd29038c97a3b1d4b0ba7dc3871d
erikbackman <erikbackman@users.noreply.github.com>
2023-06-11 22:05:29 +0200

    cleanup

--- main.lisp
+++ main.lisp
@@ -12,8 +12,6 @@
 ;; ----------------------------------------------------
 (defvar *win-h* 600)
 (defvar *win-w* 600)
-(defvar *won* nil)
-
 (defvar *board* (make-array '(3 3) :initial-element nil))
 
 (defun array-slice (arr row)
@@ -49,8 +47,8 @@
       (loop for j from 0 below m do
 	(funcall f board i j)))))
 
-(defun update-board (x y p)
-  (setf (aref *board* x y) p))
+(defun update-board (x y board p)
+  (setf (aref board x y) p))
 
 (defun clear-board (board)
   (iterate-board board (lambda (b i j) (setf (aref b i j) nil))))
@@ -116,26 +114,38 @@
     ('o
      (draw-circle (lambda (x y) (sdl2:render-draw-point renderer x y)) x0 y0 50))))
 
-(defun draw-board (renderer)
+(defun draw-board (renderer board)
   (let* ((h/3 (/ *win-h* 3))
 	 (2h/3 (* 2 h/3)))
     (sdl2:render-draw-line renderer 0 h/3 *win-w* h/3)
     (sdl2:render-draw-line renderer 0 2h/3 *win-w* 2h/3)
     (sdl2:render-draw-line renderer h/3 0 h/3 *win-h*)
     (sdl2:render-draw-line renderer 2h/3 0 2h/3 *win-h*))
-  (destructuring-bind (n m) (array-dimensions *board*)
+  (destructuring-bind (n m) (array-dimensions board)
     (loop for i from 0 below n do
       (loop for j from 0 below m do
-	(when-let ((p (aref *board* i j)))
+	(when-let ((p (aref board i j)))
 	  (let ((x0 (+ 100 (* i 200)))
 		(y0 (+ 100 (* j 200))))
 	    (draw-piece renderer x0 y0 p)))))))
 
 ;; ----------------------------------------------------
 
-(defun clear (renderer)
+(defun render (renderer board)
   (sdl2:set-render-draw-color renderer #x00 #x00 #x00 #x00)
-  (sdl2:render-clear renderer))
+  (sdl2:render-clear renderer)
+  (sdl2:set-render-draw-color renderer #xFF #xFF #xFF #xFF)
+  (draw-board renderer board)
+  (sdl2:render-present renderer))
+
+(defun update (won board)
+  (unless won
+    (multiple-value-bind (mx my btn) (sdl2::mouse-state)
+      (destructuring-bind (x . y) (pos->index mx my)
+	(update-board x y board (case btn
+				  (1 'x)
+				  (4 'o)))
+	(values (check-victory board) board)))))
 
 (defmacro with-window-renderer ((window renderer) &body body)
   `(sdl2:with-init (:video)
@@ -149,33 +159,32 @@
 	 ,@body))))
 
 (defun game-loop ()
-  (with-window-renderer (window renderer)
-    (sdl2:with-event-loop (:method :poll)
-      (:quit () t)
-      (:wait () t)
-      (:mousebuttondown ()
-			(unless *won*
-			  (multiple-value-bind (mx my btn) (sdl2::mouse-state)
-			    (destructuring-bind (x . y) (pos->index mx my)
-			      (update-board x y (case btn
-						  (1 'x)
-						  (4 'o)))
-			      (when (check-victory *board*)
-				(setf *won* t))))))
-      (:idle ()
-	     (if *won*
-		 (progn
-		   (clear renderer)
-		   (sdl2:set-render-draw-color renderer #xFF #x00 #x00 #x00))
-		 (progn
-		   (clear renderer)
-		   (sdl2:set-render-draw-color renderer #xFF #xFF #xFF #xFF)))
-	     
-	     (draw-board renderer)
-	     (sdl2:delay 60)
-	     (sdl2:render-present renderer)))))
+  (let ((won nil))
+    (with-window-renderer (window renderer)
+      (sdl2:with-event-loop (:method :poll)
+	(:quit () t)
+	(:wait () t)
+	(:mousebuttondown ()
+			  (unless won
+			    (multiple-value-bind (w) (update won *board*)
+			      (setf won w))))
+	(:idle ()
+	       (render renderer *board*)
+
+	       (when won
+		 (sdl2-ffi.functions:sdl-show-simple-message-box
+		  sdl2-ffi:+sdl-messagebox-information+
+		  "Info" "You won" window)
+		 (clear-board *board*)
+		 (setf won nil))
+	       
+	       (sdl2:delay 60))))))
 
 (defun main ()
   (clear-board *board*)
-  (setf *won* nil)
   (game-loop))
+
+(main)
+
+
+
